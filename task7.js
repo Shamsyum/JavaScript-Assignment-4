b91891b27153a77c4aaab9043f7935c20c009cf2
Shams Ur Rahman <shams.programmer@gmail.com>
2019-11-10 15:58:56 +0500

    Updated task7.js

--- task7.js
+++ task7.js
@@ -1,5 +1,10 @@
 var str = "the quick brown fox jumps over the lazy dog";
-do{
-    newString = str.slice(str.indexOf(the))
-}while()
-   
+var accurance = 0;
+for(var i=0; i<str.length; i++){
+    if(str[i] !== str.length-1){
+        if(str[i] === str.indexOf("the")){
+        accurance++;
+        }
+    }
+}
+console.log("There are "+accurance+" accurance of the word 'the'");
